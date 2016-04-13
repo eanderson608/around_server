5b8a6fb0dbc7afec9e7f9effff184c2131d93bac
Eric Anderson <eanderson608@gmail.com>
2016-04-12 21:29:03 -0500

    added ignore.false to babel require

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-
+require('babel-core/register')({
+        ignore: false
+});
 
 // Set default node environment to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
   // Register the Babel require hook
-  require('babel-core/register')({
-        "presets": ["es2015"]
-	});
+  require('babel-core/register');
 }
 
 // Export the application
